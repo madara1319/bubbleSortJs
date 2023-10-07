01174fbca85b97a22d9859cc44f5c599a4a8fb9e
madara1319 <mateusz.bik@op.pl>
2023-10-07 22:30:59 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+console.log("Test");
+testArray = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+let bubbleSort = (array) => {
+  let isSorted = false;
+  let unsortedUntilIndex = array.length - 1;
+  while (!isSorted) {
+    isSorted = true;
+    for (let i = 0; i < unsortedUntilIndex; i++) {
+      // console.log(`Kutas ${i} to ${testArray[i]}`);
+      if (array[i] > array[i + 1]) {
+        let buffer = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = buffer;
+        isSorted = false;
+      }
+    }
+    unsortedUntilIndex--;
+    //   console.log(`Just a look at array ${array}`);
+  }
+  return array;
+};
+console.log(`Array looks like this before ${testArray}`);
+bubbleSort(testArray);
+console.log(`Array looks like this after ${testArray}`);
+
+const dispArray = document.querySelector(".showArray");
+const entryInput = document.querySelector("textArea");
+
+entryInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    localStorage.setItem("entry", entryInput.value);
+    let sortedArray = bubbleSort(entryInput.value);
+
+    console.log(Array.isArray(sortedArray));
+    console.log(sortedArray);
+    bubbleSort(sortedArray);
+    console.log(sortedArray);
+    dispArray.innerHTML = `Sorted array looks like this ${sortedArray}`;
+  }
+});
+
+// entryInput.value = localStorage.getItem("entry");
